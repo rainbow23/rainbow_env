4e059831781da20fff8882af89efd35b83871aa5
futohashi <chicago_bulls_eye@hotmail.co.jp>
2018-03-24 21:50:40 +0900

    autojump修正

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,40 +3,77 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-package 'httpd'
 
-service 'httpd' do
-    action [:enable, :start]
+group 'testchef' do
+  group_name 'testchef'
+  gid        403
+  action     [:create]
 end
 
-group 'rainbow'
-
-user 'rainbow' do
-    group  'rainbow'
-    system true
-    shell  '/bin/bash'
+user 'testchef' do
+  comment  'testchef'
+  uid      403
+  group    'testchef'
+  home     '/home/testchef'
+  shell    '/bin/bash'
+  system true
+  password nil
+  manage_home true
+  # action   [:create, :manage]
+  action   [:create]
 end
 
-# template '/var/www/html/index.html' do # ~FC033
-#     source 'index.html.erb'
-#     mode   '0644'
-#     owner  'rainbow'
-#     group  'rainbow'
+# user node['deploy_user'] do
+#   action :create
+#   comment 'Application deploy user'
+#   home "/home/#{node['deploy_user']}"
+#   shell '/bin/bash'
+#   system true
+#   manage_home true
 # end
 
-git '/tmp/mysources' do
-  repository 'https://github.com/wting/autojump.git'
-  revision 'master'
-  action :sync
+directory '/home/testchef/mysources' do
+  owner 'testchef'
+  group 'testchef'
+  mode '0755'
+  action :create
 end
 
-bash 'install_something' do
-  user 'rainbow'
-  cwd '/tmp/mysources'
-  code <<-EOH
-  bash
-  ./install.py
-  sed -i -e '$a[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh' /home/rainbow/.bashrc
-  sed -i -e '$a[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh' /home/rainbow/.bash_profile
-  EOH
-end
+# git '/home/testchef/mysources' do
+#   repository 'https://github.com/wting/autojump.git'
+#   revision 'master'
+#   action :sync
+# end
+
+# bash 'install_autojump' do
+#   user 'testchef'
+#   cwd '/tmp/mysources/autojump'
+#   code <<-EOH
+#   bash
+#   ./install.py
+#   EOH
+# end
+
+# git '/home/testchef' do
+#   repository 'https://github.com/testchef/dotfiles.git'
+#   revision 'master'
+#   action :sync
+# end
+
+# # シンボリックリンクを作成
+# link '/home/testchef/_vimrc' do
+#   to '/home/testchef/.vimrc'
+# end
+
+# link '/home/testchef/vimrepos' do
+#   to '/home/testchef/_.vim'
+# end
+
+# link '/home/testchef/_tmux.conf' do
+#   to '/home/testchef/.tmux.conf'
+# end
+
+# link '/home/testchef/_zshrc' do
+#  to '/home/testchef/.zshrc'
+# end
+
